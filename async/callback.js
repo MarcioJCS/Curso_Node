164681d8ac9cae319a27b4b13bc3799917de4001
MarcioJCS <marciocruz_8612@hotmail.com>
2021-01-29 04:08:36 -0300

    Async/Await,Promise

---
+++ async/callback.js
@@ -0,0 +1,15 @@
+function soma(x, callback){
+    return setTimeout( ()=> {
+
+        return callback(null, x + 5000);
+    },3000);
+}
+
+
+//callback function
+function resolveSoma(err, resultado){
+    if(err) throw err;
+    console.log(resultado);
+}
+
+soma(200,resolveSoma)
